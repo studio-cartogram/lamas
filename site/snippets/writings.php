67588a01dfb2263ee93176411f4f138215899dd7
feeohnah <fiona@studiocartogram.com>
2023-01-26 18:52:05 +0100

    writings page was getting all posts instead of just writing

--- site/snippets/writings.php
+++ site/snippets/writings.php
@@ -4,25 +4,23 @@ $date = null;
 
 echo '<div class="scene__element scene__element--fadein page--writing ">';
 
-
-    foreach(($pages->children())->sortBy('date', 'desc') as $p):
+    foreach ($writings as $writing):
 
         echo '<div class="row writing">';
 
-        if($date === null || $date !== $p->date('Y')) :
+        if($date === null || $date !== $writing->date('Y')) :
 
-            $date = $p->date()->toDate('Y');
+            $date = $writing->date()->toDate('Y');
 
             echo '<div class="writing__date"><span class="zeta">' . $date . '</span></div>';
 
         endif;
 
-        snippet('writing', array('p' => $p));
+            snippet('writing', array('p' => $writing));
 
         echo '</div>';
 
     endforeach;
-
 
 echo '</div>';
 ?>
