fbbcf21c198436efb3f9d7f44149b9088ddf6c2b
feeohnah <fiona@studiocartogram.com>
2023-01-26 19:03:15 +0100

    Logo wasn’t clickable on Thinking page because it thought it was the homepage

--- site/snippets/brand.php
+++ site/snippets/brand.php
@@ -6,21 +6,11 @@ echo' <div class=" brand">';
 
         echo '<div class="columns medium-6 scene__element scene__element--fadein">';
 
-                if($isHomeMenu) {
-
-                    echo '<span class="wordmark rand-color">' . $site->title()->html() . '</span>';
-
-                } else {
-
                 echo '<a href="' . $site->url() . '" class=" rand-color-on-hover link--menu">';
 
                     echo '<span class="wordmark rand-color">' . $site->title()->html() . '</span>';
 
                 echo '</a>';
-
-                }
-
-
 
         echo '</div>';
 
